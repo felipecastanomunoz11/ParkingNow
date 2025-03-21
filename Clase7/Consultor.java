94ac9a75ca770a67060da0f74d62c4dc0b519211
felipecastanomunoz11 <125933191+felipecastanomunoz11@users.noreply.github.com>
2025-03-21 00:36:36 +0000

    Consultor

---
+++ Clase7/Consultor.java
@@ -0,0 +1,24 @@
+public class Consultor extends Trabajdor{
+
+    private double comision;
+      public final double SALARIO_BASE = 3500.0;// constructor
+
+      public Consultor(int idTrabajador, string nombre, string apellido, double comision){
+        super(idTrabajador, nombre, apellido);
+        this.comision = comision;
+
+        public double pagar(){
+            double total =0.0
+            total = SALARIO_BASE +(SALARIO_BASE * (comision/100));
+
+
+        public string tostring(){
+        return "consultor: " + super.getidTrabajador() + Informacion + I
+        "Nombre " + super.getNombre() +
+        "Apellido: " + super.getApellido() +
+        " Horas Laboradas: " + horas + "}";
+
+        }
+      }
+    
+}
